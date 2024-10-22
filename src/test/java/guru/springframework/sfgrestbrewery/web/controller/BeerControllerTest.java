cf9ecd310fe013b6f49d3db553f480fff6e69712
Kim Long Ngo <ngokimlong2007@gmail.com>
2024-10-22 16:51:41 -0400

    Add UT tests

--- src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/sfgrestbrewery/web/controller/BeerControllerTest.java
@@ -3,6 +3,8 @@ package guru.springframework.sfgrestbrewery.web.controller;
 import guru.springframework.sfgrestbrewery.bootstrap.BeerLoader;
 import guru.springframework.sfgrestbrewery.services.BeerService;
 import guru.springframework.sfgrestbrewery.web.model.BeerDto;
+import guru.springframework.sfgrestbrewery.web.model.BeerPagedList;
+import guru.springframework.sfgrestbrewery.web.model.BeerStyleEnum;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,10 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.List;
+import java.util.Random;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,7 +45,7 @@ class BeerControllerTest {
     }
 
     @Test
-    void getBeerById() {
+    void testGetBeerById() {
         UUID beerId = UUID.randomUUID();
         given(beerService.getById(any(), any())).willReturn(validBeer);
 
@@ -51,5 +57,52 @@ class BeerControllerTest {
                      .isOk()
                      .expectBody(BeerDto.class)
                      .value(beerDto -> beerDto.getBeerName(), Matchers.equalTo(validBeer.getBeerName()));
+    }
+
+    @Test
+    public void testGetBeerByUpc() {
+        String upc = String.valueOf(System.currentTimeMillis());
+
+        given(beerService.getByUpc(any())).willReturn(validBeer);
+
+        webTestClient.get()
+                     .uri("/api/v1/beerUpc/" + upc)
+                     .accept(MediaType.APPLICATION_JSON)
+                     .exchange()
+                     .expectStatus()
+                     .isOk()
+                     .expectBody(BeerDto.class)
+                     .value(beerDto -> beerDto.getBeerName(), Matchers.equalTo(validBeer.getBeerName()));
+    }
+
+    @Test
+    public void testGetListBeers() {
+        BeerDto beer1 = BeerDto.builder()
+                               .beerName("No Hammers On The Bar")
+                               .beerStyle(BeerStyleEnum.WHEAT.toString())
+                               .upc(BeerLoader.BEER_3_UPC)
+                               .price(new BigDecimal(BigInteger.valueOf(new Random().nextInt(10000)), 2))
+                               .quantityOnHand(new Random().nextInt(5000))
+                               .build();
+
+        BeerDto beer2 = BeerDto.builder()
+                               .beerName("Mango Bobs")
+                               .beerStyle(BeerStyleEnum.ALE.toString())
+                               .upc(BeerLoader.BEER_1_UPC)
+                               .price(new BigDecimal(BigInteger.valueOf(new Random().nextInt(10000)), 2))
+                               .quantityOnHand(new Random().nextInt(5000))
+                               .build();
+
+        BeerPagedList beerPagedList = new BeerPagedList(List.of(beer1, beer2));
+
+        given(beerService.listBeers(any(), any(), any(), any())).willReturn(beerPagedList);
+
+        webTestClient.get()
+                     .uri("/api/v1/beer")
+                     .accept(MediaType.APPLICATION_JSON)
+                     .exchange()
+                     .expectStatus()
+                     .isOk()
+                     .expectBody(BeerPagedList.class);
     }
 }
